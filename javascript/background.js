41fedc2c1c471a75f68401ff202372be6a69cc8e
namans511 <srivastava.naman511@gmail.com>
2020-08-30 22:27:51 +0530

    added ball
    restructured all the files and added ball to the main canvas

--- javascript/background.js
+++ javascript/background.js
@@ -1,8 +1,22 @@
-var canvas = document.getElementById("back");
-var ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+var backCanvas = document.getElementById("backCanvas");
+var backctx = backCanvas.getContext("2d");
+backCanvas.width = window.innerWidth;
+backCanvas.height = window.innerHeight;
 
-ctx.translate(canvas.width / 2, (2 * canvas.height) / 3);
-ctx.arc(0, 0, 100, 0, 2 * Math.PI);
-ctx.stroke();
+//draw circle
+backctx.translate(backCanvas.width / 2, (2 * backCanvas.height) / 3);
+backctx.arc(0, 0, 100, 0, 2 * Math.PI);
+backctx.stroke();
+
+//draw line through circle
+backctx.moveTo(0, 100);
+backctx.lineTo(0, -100);
+backctx.stroke();
+
+//draw horizontal lines
+backctx.moveTo(0, 100);
+backctx.lineTo(400, 100);
+backctx.stroke();
+backctx.moveTo(0, -100);
+backctx.lineTo(400, -100);
+backctx.stroke();
