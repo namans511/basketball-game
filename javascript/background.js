810c0de75305214199d9911aa77d6a297dca09a0
Dhruv Sahni <dhruvsahni89@gmail.com>
2020-08-31 00:20:49 +0530

    making court base with canvas

--- javascript/background.js
+++ javascript/background.js
@@ -2,9 +2,33 @@ var backCanvas = document.getElementById("backCanvas");
 var backctx = backCanvas.getContext("2d");
 backCanvas.width = window.innerWidth;
 backCanvas.height = window.innerHeight;
+//backCanvax.width = window.innerWidth;
+//ckCanvax.height = window.innerHeight;
 
 //draw circle
-backctx.translate(backCanvas.width / 2, (2 * backCanvas.height) / 3);
+backctx.translate(backCanvas.width / 1.6, (2 * backCanvas.height) / 3);
+backctx.arc(0, 0, 100, 0, 2 * Math.PI);
+backctx.stroke();
+
+//draw line through circle
+backctx.moveTo(0, 100);
+backctx.lineTo(0, -100);
+backctx.lineWidth=2.5;
+backctx.stroke();
+
+//draw horizontal lines
+backctx.moveTo(0, 100);
+backctx.lineTo(350, 100);
+backctx.stroke();
+backctx.moveTo(0, -100);
+backctx.lineTo(350, -100);
+backctx.stroke();
+
+//another symmetrical 
+
+//draw circle
+backctx.moveTo(-200,0);
+ backctx.translate(-300, 0);
 backctx.arc(0, 0, 100, 0, 2 * Math.PI);
 backctx.stroke();
 
@@ -15,8 +39,8 @@ backctx.stroke();
 
 //draw horizontal lines
 backctx.moveTo(0, 100);
-backctx.lineTo(400, 100);
+backctx.lineTo(-350, 100);
 backctx.stroke();
 backctx.moveTo(0, -100);
-backctx.lineTo(400, -100);
+backctx.lineTo(-350, -100);
 backctx.stroke();
