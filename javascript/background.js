3a3ed28a6be9a62455150caee27e5cb2a9ed7ed5
Dhruv Sahni <dhruvsahni89@gmail.com>
2020-08-31 20:38:24 +0530

    adding pole and changing color theme

--- javascript/background.js
+++ javascript/background.js
@@ -6,7 +6,10 @@ backCanvas.height = window.innerHeight;
 //ckCanvax.height = window.innerHeight;
 
 //draw circle
-backctx.translate(backCanvas.width / 1.6, (2 * backCanvas.height) / 3);
+backctx.beginPath();
+backctx.fillStyle='07689f';
+backctx.fill();
+backctx.translate(backCanvas.width / 1.8, (2 * backCanvas.height) / 3);
 backctx.arc(0, 0, 100, 0, 2 * Math.PI);
 backctx.stroke();
 
@@ -28,7 +31,7 @@ backctx.stroke();
 
 //draw circle
 backctx.beginPath();
- backctx.translate(-300, 0);
+ backctx.translate(-240, 0);
 backctx.arc(0, 0, 100, 0, 2 * Math.PI);
 backctx.stroke();
 
@@ -43,4 +46,44 @@ backctx.lineTo(-350, 100);
 backctx.stroke();
 backctx.moveTo(0, -100);
 backctx.lineTo(-350, -100);
+backctx.stroke();
+
+//pole
+backctx.beginPath();
+backctx.fillStyle='red';
+backctx.fillRect(660, 0, 150, 200);
+backctx.fillStyle='gray';
+backctx.moveTo(733,0);
+backctx.lineTo(733,-400);
+backctx.lineWidth=15;
+backctx.strokeStyle='#2d4059';
+backctx.stroke();
+
+backctx.beginPath();
+backctx.moveTo(733,-360);
+backctx.lineTo(690,-360);
+backctx.lineWidth=8;
+backctx.strokeStyle='#2d4059';
+backctx.stroke();
+
+backctx.beginPath();
+backctx.moveTo(733,-300);
+backctx.lineTo(690,-300);
+backctx.lineWidth=8;
+backctx.strokeStyle='#2d4059';
+backctx.stroke();
+
+
+backctx.beginPath();
+backctx.moveTo(690,-400);
+backctx.lineTo(690,-250);
+backctx.lineWidth=8;
+backctx.strokeStyle='#2d4059';
+backctx.stroke();
+
+backctx.beginPath();
+backctx.moveTo(688,-300);
+backctx.lineTo(570,-300);
+backctx.lineWidth=9;
+backctx.strokeStyle='orange';
 backctx.stroke();
