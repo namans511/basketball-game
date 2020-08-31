92b2092b25e62876304b686471e19890142a3558
namans511 <srivastava.naman511@gmail.com>
2020-08-31 23:39:27 +0530

    added ball movement on space press

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,29 @@ ball.src = "images/ball.png";
 ball.onload = function () {
   ctx.drawImage(ball, 50, 400, 100, 100);
 };
+
+var id;
+var keyPressed = false;
+var ypos = 2;
+var xpos = 2;
+
+//moving ball on space press
+function spacePress(event) {
+  if (event.keyCode == 32 && keyPressed == false) {
+    keyPressed = true;
+    id = setInterval(function () {
+      ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      ctx.drawImage(ball, 50 + xpos, 400 - ypos, 100, 100);
+      ypos += 2;
+      xpos += 2;
+    }, 20);
+  }
+  // console.log("start");
+}
+
+//stopping ball on space realse
+function spaceRelease(event) {
+  clearInterval(id);
+  keyPressed = false;
+  // console.log("clear");
+}
